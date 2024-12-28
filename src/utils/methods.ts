f664396d79fe1f669ff350e9b13667ed52bbc56d
imkishan15 <kalavadiakishan15@gmail.com>
2024-12-28 22:46:27 +0530

    Fixed few bugs

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -8,12 +8,6 @@ export const getAllLaunchpads = async (): Promise<LaunchPad[]> =>
 export const getOneLaunchpad = async (id: string): Promise<LaunchPad> =>
   (await axios.get(`${baseUrl}/launchpads/${id}`)).data;
 
-export const getOneRocket = async (id: string) =>
-  (await axios.get(`${baseUrl}/rockets/${id}`)).data;
-
-export const getRocjketsByIds = async (ids: string[]) =>
-  await Promise.all(ids.map((id) => getOneRocket(id)));
-
 export const getRocketsByIds = async (ids: string[]): Promise<Rocket[]> =>
   await Promise.all(
     ids.map(async (id) => (await axios.get(`${baseUrl}/rockets/${id}`)).data)
